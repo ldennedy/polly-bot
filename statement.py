d806f91905d177ebc35056d306ee33a8f2e7337a
Lauren Dennedy <laurendennedy@gmail.com>
2019-10-10 15:23:07 -0700

    Added comments in statement.py

--- statement.py
+++ statement.py
@@ -1,10 +1,13 @@
 class Statement(object):
+    # A statement object contains a string for the sentence
+    # and a list of words that fit the "context"
+    # (What words were used in previous message for this statement)
     sentence = ""
     context = []
     def __init__(self, s, c = None):
         self.sentence = s
         if c:
             self.generate_context(c)
-
+            
     def generate_context(self, c):
         self.context = c.split()
